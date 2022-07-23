e08b7bb20015c45a87b8ea0d647429fdf0a39fbf
jingchaoyang <jingchaoyang@autohome.com.cn>
2022-07-23 21:44:18 +0800

    初始化项目网易云

---
+++ 02_LEARN_VUE3/musicapp/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <div id="d1">
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
+<style lang="less" scoped>
+#d1 {
+  width: 3.75rem;
+  height: 3.75rem;
+  background: red;
+}
+</style>
