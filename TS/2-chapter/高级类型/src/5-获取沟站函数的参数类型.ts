5ec87c676f9e93d71a17a80da752e061ac4053f4
jingchaoyang <jingchaoyang@autohome.com.cn>
2022-11-30 22:00:03 +0800

    infer和构造函数的应用

---
+++ TS/2-chapter/高级类型/src/5-获取沟站函数的参数类型.ts
@@ -0,0 +1,33 @@
+class TestClass {// 准备类
+  public name: string
+  public classno: number
+  constructor(name: string, classno: number) {
+    this.name = name;
+    this.classno = classno
+  }
+  eat() {
+    console.log("姓名为: " + this.name);
+  }
+}
+
+type ConstructorParametersType<T extends new (...args: any[]) => any>
+  = T extends new (...args: infer P) => any ? P : never
+// 获取到了 TestClass 构造函数的参数类型 放到一个元组中[name: string, classno: number]
+
+// let constructorParameters: ConstructorParametersType<typeof TestClass>
+// let constructorParameters: 
+//  ConstructorParametersType<new (name: string, classno: number) => TestClass>
+let constructorParameters: ConstructorParametersType<typeof TestClass>
+// typeof TestClass // TODO: 是要构造器的类型，而不是类
+type Constructor<T> = new (...args: any[]) => T
+
+
+
+
+
+
+
+
+
+
+
