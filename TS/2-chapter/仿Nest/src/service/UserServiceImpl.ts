dbf816272a037a6d7fd4f0240b24e2a226c2b948
jingchaoyang <jingchaoyang@autohome.com.cn>
2022-12-18 14:12:18 +0800

    仿nest准备

---
+++ TS/2-chapter/仿Nest/src/service/UserServiceImpl.ts
@@ -0,0 +1,21 @@
+// TODO: 这个函数就是被注入的
+export default class UserServiceImpl {
+
+  constructor() {
+    console.log("UserServiceImpl构造器....");
+  }
+
+  Login(username: string, pwd: string, role: string) {
+    console.log("进入service ...Login,username:", username)
+
+    if (username === "admin" && pwd === "123" && role === "admin") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  register() {
+    console.log("usersevice...register")
+  }
+}
