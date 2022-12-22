631203eae2b19538ea7070f0f85de77301360695
jingchaoyang <jingchaoyang@autohome.com.cn>
2022-12-22 15:18:54 +0800

    路由器泛型

--- TS/2-chapter/仿Nest/知识补充/GetRouteParameter.ts
+++ TS/2-chapter/仿Nest/知识补充/GetRouteParameter.ts
@@ -3,3 +3,5 @@ export type GetRouteParameter<S extends string> = RemoveTail<
   RemoveTail<RemoveTail<S, `/${string}`>, `-${string}`>,
   `.${string}`
 >;
+
+// TODO:  这个参考2RemoveTail 逐层去除把字符串，先去掉 /  再去掉 - 最后去掉 .
