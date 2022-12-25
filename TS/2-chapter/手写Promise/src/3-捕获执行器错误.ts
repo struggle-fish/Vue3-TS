098b6b6a3d213a0a8744366da4f149934809a786
jingchaoyang <jingchaoyang@autohome.com.cn>
2022-12-25 14:21:48 +0800

    手写promise 捕获执行器错误

---
+++ TS/2-chapter/手写Promise/src/3-捕获执行器错误.ts
@@ -0,0 +1,70 @@
+import { RejectType, ResolveType, Executor } from './actiontype'
+
+
+
+
+
+export default class Promise<T = any> {
+  resolve!: ResolveType
+  
+  reject!: RejectType
+  status!: string
+  resolve_executor_value!: any
+  reject_executor_value!: any
+
+
+  constructor(executor: Executor) {
+    this.status = 'pending'
+
+    this.resolve = (value: any) : any => {
+      if (this.status === 'pending') {
+        this.status = 'success'
+        // value[10] = 100
+        this.resolve_executor_value = value
+        console.log('resolve==>value', value)
+      }
+      
+    }
+
+    this.reject = (value: any) : any => {
+      if (this.status === 'pending') {
+        this.status = 'fali'
+        this.reject_executor_value = value
+        console.log('reject==>value', value)
+      }
+    }
+
+    try {
+      executor(this.resolve, this.reject)
+    } catch (error) {
+      this.status = 'pending'
+      this.reject(error?.toString())
+      throw new Error('执行器报错')
+    }
+  
+  }
+
+
+
+  then(resolveInthen: ResolveType, rejectInthen: RejectType) {
+    if (this.status === 'success') {
+      resolveInthen(this.resolve_executor_value)
+    }
+
+    if (this.status === 'fail') {
+      rejectInthen(this.reject_executor_value)
+    }
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+export {}
