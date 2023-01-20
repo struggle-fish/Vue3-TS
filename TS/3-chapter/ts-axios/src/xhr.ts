da5390a9b5365dff0fe7bf796de63f00bc92cf6f
jingchaoyang <jingchaoyang@autohome.com.cn>
2023-01-20 20:39:21 +0800

    添加了错误捕获

--- TS/3-chapter/ts-axios/src/xhr.ts
+++ TS/3-chapter/ts-axios/src/xhr.ts
@@ -2,13 +2,24 @@ import { parseHeaders } from './helpers/headers'
 import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from './types'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
-  return new Promise(resolve => {
-    const { data = null, url, method = 'get', headers, responseType } = config
+  return new Promise((resolve, reject) => {
+    const {
+      data = null,
+      url,
+      method = 'get',
+      headers,
+      responseType,
+      timeout
+    } = config
 
     const request = new XMLHttpRequest()
 
     if (responseType) {
       request.responseType = responseType
+    }
+
+    if (timeout) {
+      request.timeout = timeout
     }
 
     request.open(method.toUpperCase(), url, true)
@@ -17,6 +28,11 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       if (request.readyState !== 4) {
         return
       }
+
+      if (request.status === 0) {
+        return
+      }
+
       const reponseHeaders = parseHeaders(request.getAllResponseHeaders())
       const responseData =
         responseType !== 'text' ? request.response : request.responseText
@@ -29,7 +45,15 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         config,
         request
       }
-      resolve(response)
+      handleResponse(response)
+    }
+
+    request.onerror = function handleError() {
+      reject(new Error('网络错误'))
+    }
+
+    request.ontimeout = function handleTimeout() {
+      reject(new Error(`timeout ${timeout} ms`))
     }
 
     Object.keys(headers).forEach(name => {
@@ -40,5 +64,13 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       }
     })
     request.send(data)
+
+    function handleResponse(response: AxiosResponse): void {
+      if (response.status >= 200 && response.status < 300) {
+        resolve(response)
+      } else {
+        reject(new Error(`状态码错误${response.status}`))
+      }
+    }
   })
 }
