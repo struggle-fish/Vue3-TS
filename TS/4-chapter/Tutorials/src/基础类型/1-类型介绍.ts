1505cafff1793da5ada66d92aa41d89bbca0949f
jingchaoyang <jingchaoyang@autohome.com.cn>
2023-02-09 14:21:42 +0800

    基础类型

---
+++ TS/4-chapter/Tutorials/src/基础类型/1-类型介绍.ts
@@ -0,0 +1,16 @@
+// @see https://www.tslang.cn/docs/handbook/basic-types.html
+
+// 1、布尔值
+// 2、数字
+// 3、字符串
+// 4、数组
+// 5、元组 Tuple
+// 6、枚举
+// 7、Any
+// 8、Void
+// 9、Null
+// 10、Undefined
+// 11、Never
+// 12、Object
+// 13、类型断言
+
