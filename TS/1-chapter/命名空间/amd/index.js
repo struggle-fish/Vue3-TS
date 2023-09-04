ac08e76925d7760471af20264218e59c1d1e52ae
jingchaoyang <jingchaoyang@autohome.com.cn>
2023-09-04 10:02:09 +0800

    同步代码

---
+++ TS/1-chapter/命名空间/amd/index.js
@@ -0,0 +1,5 @@
+// 学习 TS 就是学习 TS中的类型
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+});
---
+++ TS/1-chapter/命名空间/src/index.js
@@ -0,0 +1,3 @@
+"use strict";
+// 学习 TS 就是学习 TS中的类型
+Object.defineProperty(exports, "__esModule", { value: true });
